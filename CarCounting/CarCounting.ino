86f33a3faaf93c4fbc4efec44c69b92722fccbae
Zachary <zjod95@gmail.com>
2018-02-02 16:12:42 -0500

    More changes adding in BLE and time sync

--- CarCounting/CarCounting.ino
+++ CarCounting/CarCounting.ino
@@ -1,32 +1,52 @@
 #include <DistanceGP2Y0A21YK.h>
+#include <SoftwareSerial.h>
 #include <elapsedMillis.h>
 #include <QueueList.h>
 #include <TimeLib.h>
 #include <SD.h>
 
+#define _DEBUG_
+
+// General use definitions and declarations
 #define TIME_HEADER  "T"   // Header tag for serial time sync message
 #define TIME_REQUEST  7    // ASCII bell character requests a time sync message 
 
 DistanceGP2Y0A21YK Dist;
+SoftwareSerial BTSerial(2, 3);  // SET PINS APPROPRIATELY
 File saveFile;
-const int DeviceID = 0;
 
+// Device specific information
+String DeviceID;
+String DeviceLoc;
+String DeviceDir;
+String DevComment;
+// NEED TO STORE TO FILE ON SD FOR RETRIEVAL
+
+// Timer
 elapsedMillis timer0;
 bool timer0up;
+bool synced;
 
+// Regarding the sensor
 int distance;
 int firstDist;
 int lastDist;
 int avgDist;
 
+// Regarding BLE/Serial
+String msgSer;
+int state = 0;
+
+// Regarding the SD card
 QueueList<String> queue;
-String msg;
+String msgSD;
 String filename;
 
 // SET PINS APPROPRIATELY
 int sensorPin = A1;
 const int chipSelect = 4;
 
+// Serial sync
 void processSyncMessage() {
   unsigned long pctime;
   const unsigned long DEFAULT_TIME = 1357041600; // Jan 1 2013
@@ -36,15 +56,22 @@ void processSyncMessage() {
      if( pctime >= DEFAULT_TIME) { // check the integer is a valid time (greater than Jan 1 2013)
        setTime(pctime); // Sync Arduino clock to the time received on the serial port
      }
+  } else if(BTSerial.find(TIME_HEADER)) {
+     pctime = BTSerial.parseInt();
+     if( pctime >= DEFAULT_TIME) { // check the integer is a valid time (greater than Jan 1 2013)
+       setTime(pctime); // Sync Arduino clock to the time received on the serial port
+     }
   }
 }
 
 time_t requestSync()
 {
-  Serial.write(TIME_REQUEST);  
+  Serial.write(TIME_REQUEST);
+  BTSerial.write(TIME_REQUEST);  
   return 0; // the time will be sent later in response to serial mesg
 }
 
+// SD functions
 void SDInit() {
   if (!SD.begin(chipSelect)) {
     Serial.println("SD Initialization Failed!");
@@ -52,7 +79,13 @@ void SDInit() {
   }
   Serial.println("SD Initialization Done.");
 
+  // SAVE DEVICE VARIABLES
+
   filename = DeviceID;
+  filename += "_";
+  filename = DeviceLoc;
+  filename += "_";
+  filename = DeviceDir;
   filename += "_";
   filename += now(); 
   filename += ".txt";
@@ -65,6 +98,10 @@ void swapFiles() {
   saveFile.close();
   filename = DeviceID;
   filename += "_";
+  filename = DeviceLoc;
+  filename += "_";
+  filename = DeviceDir;
+  filename += "_";
   filename += now(); 
   filename += ".txt";
   char temp[filename.length()+1];
@@ -72,18 +109,99 @@ void swapFiles() {
   saveFile = SD.open(temp, FILE_WRITE);
 }
 
+// BLE connection
+void setParamsCommands() {
+  while (Serial.available() > 0)   {
+    msgSer = Serial.readString();
+  }
+
+  if (msgSer.indexOf("ID=") > 0) {
+          #ifdef _DEBUG_
+            Serial.print(">>Received command: ");
+            Serial.println(msgSer);
+          #endif
+
+          msgSer.remove(0, 2);
+          DeviceID = msgSer;
+  }
+  else if (msgSer.indexOf("LOC=") > 0) {
+          #ifdef _DEBUG_
+            Serial.print(">>Received command: ");
+            Serial.println(msgSer);
+          #endif
+
+          msgSer.remove(0, 3);
+          DeviceLoc = msgSer;
+  }
+  else if (msgSer.indexOf("DIR=") > 0) {
+          #ifdef _DEBUG_
+            Serial.print(">>Received command: ");
+            Serial.println(msgSer);
+          #endif
+
+          msgSer.remove(0, 3);
+          DeviceDir = msgSer;
+  }
+  else if (msgSer.indexOf("COMMENT=") > 0) {
+          #ifdef _DEBUG_
+            Serial.print(">>Received command: ");
+            Serial.println(msgSer);
+          #endif
+
+          msgSer.remove(0, 7);
+          DevComment = msgSer;
+  }
+}
+
+void runCommands() {
+  while (Serial.available() > 0)   {
+    msgSer = Serial.readString();
+  }
+
+  if (msgSer.indexOf("START_RUNNING") > 0) {
+          #ifdef _DEBUG_
+            Serial.print(">>Received command: ");
+            Serial.println(msgSer);
+          #endif
+  } else if (msgSer.indexOf("STOP_RUNNING") > 0) {
+          #ifdef _DEBUG_
+            Serial.print(">>Received command: ");
+            Serial.println(msgSer);
+          #endif
+  } else if (msgSer.indexOf("RETURN_DATA") > 0) {
+          #ifdef _DEBUG_
+            Serial.print(">>Received command: ");
+            Serial.println(msgSer);
+          #endif
+  }
+}
+
+// Main setup and loop
 void setup() {
   Serial.begin(9600);
+  BTSerial.begin(38400);
   Dist.begin(sensorPin);
 
-  setSyncProvider( requestSync);  //set function to call when sync required
+  setSyncProvider(requestSync);  // Set function to call when sync required
   while (timeStatus() == timeNotSet) {
-    if (Serial.available()) {
+    requestSync();
+    if (Serial.available() || BTSerial.available()) {
       processSyncMessage();
     }
   }
   timer0up = false;
   timer0 = 0;
+  
+  DeviceID = "";
+  DeviceLoc = "";
+  DeviceDir = "";
+  DevComment = "";
+
+  // CHECK FOR DEVICE VARIABLES ON DISK
+
+  while (DeviceID == "" && DeviceLoc == "" && DeviceDir == "") {
+    setParamsCommands();
+  }
   
   SDInit();
 }
@@ -98,22 +216,23 @@ void loop() {
       if (timer0up == false) {
         timer0up = true;
         timer0 = 0;
-        msg = now();
-        msg += ", ";
+        msgSD = now();
+        msgSD += ", ";
         firstDist = distance;
       } else {
         lastDist = distance;
+        // Collect every 10 samples
       }
     }
   } else {
     if (timer0up == true) {
       // Was the length of time valid (more than 100 ms)?
       if (timer0 > 100) {
-        msg += timer0;
-        msg += ", ";
+        msgSD += timer0;
+        msgSD += ", ";
         avgDist = (firstDist + lastDist) / 2;
-        msg += avgDist;
-        queue.push(msg);
+        msgSD += avgDist;
+        queue.push(msgSD);
         }
         timer0up = false; 
     } else {
@@ -128,7 +247,10 @@ void loop() {
           }
         } else {
           // If the file didn't open, print an error
-          //Serial.println("Error opening SD file");
+          #ifdef _DEBUG_
+            Serial.println("Error opening SD file");
+          #endif
+          
           Serial.println(queue.pop());
         }
       }
